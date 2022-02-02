280bdcf676cd32f6729a91ca19d5d1b6be729fef
Duda Ferreira <ferreira.mev@gmail.com>
2022-02-02 20:18:21 -0300

    Output response to GET

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const express = require("express");
+// const express = require("express");  // common JS
+import express from "express";  // ES Modules
+
+// COMMON JS ->
+// IMPORT > const express = require("express")
+// EXPORT -> module.export = { express: 123 }
+
+// ESMODULES
+// IMPORT -> import express from 'express'
+// EXPORT -> export { express: 123 } 
+
 const app = express();
 
+app.get("/", 
+    (request, response, next) => 
+    {
+        response.send("Hello world");
+    })
 // Criar serviço p/ o Express ouvir:
 
-app.listen(3000);
+app.listen(3000, () => {console.log("Server running on port 3000");});
+// esse log é do lado do servidor, não aparece no browser
