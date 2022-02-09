beaba69fbc8d301d86cff2afe5e4c90577dc3c62
Duda Ferreira <ferreira.mev@gmail.com>
2022-02-09 19:54:05 -0300

    Build shortened link schema

--- url-shortener/src/model/ShortenerModel.js
+++ url-shortener/src/model/ShortenerModel.js
@@ -3,8 +3,19 @@ import mongoose from "mongoose";
 const ShortenerSchema = mongoose.Schema
 (
     {
-        name: String
-        // to be continued
+        link: { type: String, required: true },
+        hash: { type: String, required: true },
+        expired: { type: Boolean },
+        // análogo ao atributo derivado no relacional
+        // como eu incluiria um gatilho? ou só no backend?
+        expirationDate: Date,
+        hits: { type: Number, default: 0, min: 0 },
+        // como validariamos que hits >= 0?
+        name: String,
+        metadata: mongoose.Schema.Types.Mixed
+    },
+    {
+        timestamps: true
     }
 );
 
