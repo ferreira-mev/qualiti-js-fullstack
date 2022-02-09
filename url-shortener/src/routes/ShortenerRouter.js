a9adb73f2b8b588b8c043f56bb320c3809f1f617
Duda Ferreira <ferreira.mev@gmail.com>
2022-02-09 10:11:37 -0300

    Add semicolons (:P)

--- url-shortener/src/routes/ShortenerRouter.js
+++ url-shortener/src/routes/ShortenerRouter.js
@@ -6,10 +6,10 @@ const shortenerController = new ShortenerController();
 
 const router = Router();
 
-router.get("/shortener", shortenerController.index)
-router.get("/shortener/:id", shortenerController.getOne)
-router.post("/shortener", shortenerController.store)
-router.put("/shortener/:id", shortenerController.update)
-router.delete("/shortener/:id", shortenerController.remove)
+router.get("/shortener", shortenerController.index);
+router.get("/shortener/:id", shortenerController.getOne);
+router.post("/shortener", shortenerController.store);
+router.put("/shortener/:id", shortenerController.update);
+router.delete("/shortener/:id", shortenerController.remove);
 
 export default router;
--- url-shortener/src/routes/UserRouter.js
+++ url-shortener/src/routes/UserRouter.js
@@ -5,14 +5,14 @@ import UserController from "../controller/UserController.js";
 const router = Router();
 
 // Retorna JSON com todos os usuários:
-router.get("/user", UserController.index)
+router.get("/user", UserController.index);
 
 // Retorna JSON com o usuário cujo ID é passado como
 // parâmetro na URL, caso haja:
-router.get("/user/:id", UserController.getOne)
+router.get("/user/:id", UserController.getOne);
 
 // Insere novo usuário, com ID gerado aleatoriamente:
-router.post("/user", UserController.store)
+router.post("/user", UserController.store);
 
 /* Outra entrada, p/ copiar e simular POST no Postman:
 {
@@ -24,12 +24,12 @@ router.post("/user", UserController.store)
 // Atualiza a entrada referente ao usuário cujo ID é indicado
 // no body, caso haja:
 // (HW: Retornar usuário atualizado ou 404 com mensagem)
-router.put("/user", UserController.update)
+router.put("/user", UserController.update);
 
 
 // Remove a entrada referente ao usuário cujo ID é indicado
 // no body, caso haja:
 // (HW: Retornar 200 se conseguir deletar ou 404 se não existir)
-router.delete("/user", UserController.remove)
+router.delete("/user", UserController.remove);
 
 export default router;
